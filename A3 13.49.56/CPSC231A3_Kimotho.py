cd91b89408939990cecced3368f8dc4b1f1b7aaf
Jeremy Kimotho <jeremykimotho@yahoo.com>
2019-11-11 22:43:17 -0700

    have done 90% of the read const info function. just need to add the part that returns the names of the constellation files to the consle.

--- A3 13.49.56/CPSC231A3_Kimotho.py
+++ A3 13.49.56/CPSC231A3_Kimotho.py
@@ -19,6 +19,15 @@ def screenCoor(x, y):
     y_coordinate=float(yo+(ratio*y))
     return x_coordinate, y_coordinate
 
+# The function that changes the colour of the constellation lines. It goes from red to green to yellow and cycles through those three colours
+def colour_cycler(counter):
+    if counter==0 or counter%3==0:
+      return 'red'
+    elif counter==1 or counter%3==1:
+      return 'green'
+    elif counter==2 or counter%3==2:
+      return 'yellow'
+
 #  Collects data from the command line input using the number of arguments. Checks inputted star_file to ensure it exists and then prompts for constellation files until user enters blank input. Constellation files are also checked for validity. Function then returns three variables in all situations where an error doesn't occur. These are first the star_file the user entered, then a list of the constellation files the user entered, and finally a code of 0, 1, or 2. The code determines if the star and constellations will just be drawn (0), if the star and constellation files will be drawn and the names written in the drawing window (1) or finally if the star and constellation files will be drawn and the names printed to the screen. All the situations that do not result in an error have one of the three coded outcomes.
 def command_line():
     if len(sys.argv)==1:
@@ -234,26 +243,41 @@ def star_drawing(pointer, code, stars):
                 print(star.name)
     print('Completed it!')
 
-def read_const_info():
-    const_files=[]
+# Takes as input the constellation files that were added during the command line argument collection and extends that to the list of const files it gets from the  user at the start of the function. Then loops through all the const files and makes tuples line-by-line and these tuples are the star edge or constellation edges. Lastly prints to the console the title of every constellation and the stars it connects. Returns list of tuples.
+def read_const_info(const_files):
+    const_files_2=[]
     const_file=input('Enter a constellation file: ')
     while const_file!='':
         if path.isfile(const_file)==False:
             print(f'The constellation file you entered {const_file} is invalid.')
-        const_files.append(const_file)
+        else:
+            const_files_2.append(const_file)
         const_file=input('Enter a constellation file: ')
+    all_files=const_files+const_files_2
+    processed_const_files=[]
+    for const_file in all_files:
+        try:
+            cf=open(const_file, 'r')
+            lines=cf.readlines()
+            const_names=[]
+            for i in range(1, len(lines)):
+                star_edge=lines[i].split(',')
+                const_info=(star_edge[0], star_edge[1][0:-1])
+                processed_const_files.append(const_info)
+            # Haven't got the name returning to work
+            #     const_names.append(star_edge[0])
+            #     if i==len(lines):
+            #         const_names.append(star_edge[1][0:-1])
+            # print(f'{lines[0][0:-1]} constellation contains {const_names}')
+            cf.close()
+        except IOError:
+            print(f'There was an error in opening the constellation file {const_file}')
+            sys.exit(1)
+    return processed_const_files
+    
 
 def const_drawing():
     pass
-
-# The function that changes the colour of the constellation lines. It goes from red to green to yellow and cycles through those three colours
-def colour_cycler(counter):
-    if counter==0 or counter%3==0:
-      return 'red'
-    elif counter==1 or counter%3==1:
-      return 'green'
-    elif counter==2 or counter%3==2:
-      return 'yellow'
 
 def setup():
     pointer = turtle.Turtle()
@@ -267,15 +291,16 @@ def setup():
     return pointer
 
 def main():
-    star_file, const_files, code=command_line()
-    processed_star_data=read_star_info(star_file)
-    pointer = setup()
-    #Draw Axes (function)
-    pointer.color(AXISCOLOR)
-    axis_drawing(pointer)
-    #Draw Stars (function)
-    star_drawing(pointer, code, processed_star_data)
-    read_const_info()
+    # star_file, const_files, code=command_line()
+    # processed_star_data=read_star_info(star_file)
+    # pointer = setup()
+    # # Draw Axes (function)
+    # pointer.color(AXISCOLOR)
+    # axis_drawing(pointer)
+    # # Draw Stars (function)
+    # star_drawing(pointer, code, processed_star_data)
+    const_files=['BigDipper.dat']
+    read_const_info(const_files)
     #Loop getting filenames
         #Read constellation file (function)
         #Draw Constellation (function)
